96b3c5313a651186608ac30b32b1afc4da4e1b02
Andre Moreira Magalhaes <andre@endlessm.com>
2020-03-27 11:40:36 -0300

    Revert "Properly update visibility"
    This reverts commit ea2c9ce78e4707b4afd63b6e55d08db4fe42d482.
    
    Fix BackgroundManager usage with gnome shell 3.36.
    
    https://phabricator.endlessm.com/T28780

--- extension.js
+++ extension.js
@@ -233,7 +233,7 @@ var Watermark = GObject.registerClass({
     }
 
     _updateVisibility() {
-        let background = this._bgManager.backgroundActor.background._delegate;
+        let { background } = this._bgManager.backgroundActor;
         let defaultUri = background._settings.get_default_value('picture-uri');
         let file = Gio.File.new_for_commandline_arg(defaultUri.deep_unpack());
 
